143d7dbc5d88a035a901a1d0b5f9d5006d72a304
Karwega <knjogo@outlook.com>
2023-04-20 23:46:30 +0300

    Yes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedT
     if (err) console.log(err)
 });
 
-// test if the database has connected successfully
+// test if the database has connected successfull
 let db = mongoose.connection;
 db.once('open', ()=>{
     console.log('Database connected successfully')
